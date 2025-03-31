ad9c35121144b17bc6520f15b30191576de9e1e3
Kaido3737 <kaidou.3737@gmail.com>
2025-04-01 06:57:03 +0900

    カクテル配列

---
+++ src/data/cocktails.ts
@@ -0,0 +1,11 @@
+import blue from './images/cocktail_blue.gif';
+import { Cocktail } from '../types/cocktail';
+
+export const cocktails: Cocktail[] = [
+  {
+    id: "blue",
+    name: "blue",
+    image: blue,
+    description: "blue",
+  },
+];
