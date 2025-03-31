66fb61f5a3204c1c4b2f7d577931045addc98f4a
Kaido3737 <kaidou.3737@gmail.com>
2025-04-01 06:57:11 +0900

    ホームページ実装

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import { cocktails } from '../data/cocktails';
+import CocktailCard from '../components/CocktailCard';
+
+const Home = () => {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Our Signature Cocktails</h1>
+      <div
+        style={{
+          display:  'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          gap: '2rem',
+          marginTop: '2rem',
+        }}>
+          {cocktails.map((cocktail) => (
+            <CocktailCard key={cocktail.id} cocktail={cocktail} />
+          ))}
+        </div>
+    </main>
+  );
+};
+
+export default Home;
