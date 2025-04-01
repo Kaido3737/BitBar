5566c0fbd6dd0cbbc78af1f6b8b299876b5c9978
Kaido3737 <kaidou.3737@gmail.com>
2025-04-01 13:30:19 +0900

    カードホバー時に説明を出す

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -1,5 +1,6 @@
 import { Cocktail } from '../types/cocktail';
 import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import styles from './styles/CocktailCard.module.scss';
 
 const CocktailCard = ({cocktail}: {cocktail: Cocktail}) => {
@@ -7,12 +8,17 @@ const CocktailCard = ({cocktail}: {cocktail: Cocktail}) => {
 
   return (
     <div
-      className={styles.card}
+      className={styles['card']}
       onClick={() => navigate(`/cocktail/${cocktail.id}`)}
     >
-      <img src={cocktail.image} alt={cocktail.name} />
-      <h2>{cocktail.name}</h2>
-      <p>{cocktail.description}</p>
+      <img src={cocktail.image} alt={cocktail.name} className={styles['card__image']}/>
+      <h2 className={styles['card__title']}>{cocktail.name}</h2>
+      <p className={styles['card__description']}>{cocktail.description.split('\n').map((line, i) => (
+        <React.Fragment key={i}>
+          {line}
+          <br />
+        </React.Fragment>
+      ))}</p>
     </div>
   );
 };
--- src/data/cocktails.ts
+++ src/data/cocktails.ts
@@ -1,11 +1,11 @@
-import blue from './images/cocktail_blue.gif';
+import ChinaBlue from './images/cocktail_blue.gif';
 import { Cocktail } from '../types/cocktail';
 
 export const cocktails: Cocktail[] = [
   {
     id: "blue",
-    name: "blue",
-    image: blue,
-    description: "blue",
+    name: "China Blue",
+    image: ChinaBlue,
+    description: "澄んだブルーに、ライチとグレープフルーツの香りが広がる爽やかな一杯。\nふっと気持ちを切り替えたい夜や、静かに自分と向き合いたいときにぴったり。\n甘すぎず軽やかな後味が、心をそっとリセットしてくれる。",
   },
 ];
