37a6fd446e8dd39b39722e75c742878d8ea2f7dd
Kaido3737 <kaidou.3737@gmail.com>
2025-04-04 08:14:17 +0900

    Headerアイコン追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 import styles from './styles/Header.module.scss';
+import logo from './images/logo.gif';
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <h1 className={styles.logo}>🍸 Bit Bar</h1>
+    <div className={styles['header']}>
+      <h1 className={styles['header__icon']}>
+        <img src={logo} alt="Bit Bar Logo" className={styles['header__icon--img']}/>
+        <span className={styles['header__icon--text']}>Bit Bar</span>
+      </h1>
       <nav className={styles.nav}>
         <a href="#">Home</a>
         <a href="#about">About</a>
