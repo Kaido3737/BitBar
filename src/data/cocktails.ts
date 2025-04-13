a51895b8816573b112106f16431f8f117e39a985
Kaido3737 <kaidou.3737@gmail.com>
2025-04-13 16:38:41 +0900

    カクテル追加

--- src/data/cocktails.ts
+++ src/data/cocktails.ts
@@ -1,4 +1,7 @@
 import ChinaBlue from './images/cocktail_blue.gif';
+import FuzzyNable from './images/cocktail_orange.gif';
+import Cosmopolitan from './images/cosmopolitan.gif';
+import Negroni from './images/negroni.gif';
 import { Cocktail } from '../types/cocktail';
 
 export const cocktails: Cocktail[] = [
@@ -7,5 +10,23 @@ export const cocktails: Cocktail[] = [
     name: "China Blue",
     image: ChinaBlue,
     description: "澄んだブルーに、ライチとグレープフルーツの香りが広がる爽やかな一杯。\nふっと気持ちを切り替えたい夜や、静かに自分と向き合いたいときにぴったり。\n甘すぎず軽やかな後味が、心をそっとリセットしてくれる。",
+  },
+  {
+    id: "orange",
+    name: "Fuzzy Navel",
+    image: FuzzyNable,
+    description: "aaaa",
+  },
+  {
+    id: "cosmopolitan",
+    name: "Cosmopolitan",
+    image: Cosmopolitan,
+    description: "bbbb",
+  },
+  {
+  id: "negroni",
+  name: "Negroni",
+  image: Negroni,
+  description: "cccc",
   },
 ];
