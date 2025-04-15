e63b4cff33dda532508c07adb9021e0e39ed124b
Kaido3737 <kaidou.3737@gmail.com>
2025-04-15 12:59:02 +0900

    アイコンをクリックでページ遷移

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import styles from './styles/Header.module.scss';
 import logo from './images/logo.gif';
+import { useNavigate } from 'react-router-dom';
+
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <div className={styles['header']}>
-      <h1 className={styles['header__icon']}>
+      <h1 className={styles['header__icon']} onClick={() => navigate(`/`)}>
         <img src={logo} alt="Bit Bar Logo" className={styles['header__icon--img']}/>
         <span className={styles['header__icon--text']}>Bit Bar</span>
       </h1>
