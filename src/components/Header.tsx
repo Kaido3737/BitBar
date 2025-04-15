893792a2944d51ea0295c94b96974a55f159d8d7
Kaido3737 <kaidou.3737@gmail.com>
2025-04-15 22:59:39 +0900

    url変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,12 +7,12 @@ const Header = () => {
   const navigate = useNavigate();
   return (
     <div className={styles['header']}>
-      <h1 className={styles['header__icon']} onClick={() => navigate(`/`)}>
+      <h1 className={styles['header__icon']} onClick={() => navigate(`/BitBar/`)}>
         <img src={logo} alt="Bit Bar Logo" className={styles['header__icon--img']}/>
         <span className={styles['header__icon--text']}>Bit Bar</span>
       </h1>
       <nav className={styles.nav}>
-        <a href="" onClick={() => navigate(`/`)}>Home</a>
+        <a href="" onClick={() => navigate(`/BitBar/`)}>Home</a>
         <a href="#about">About</a>
       </nav>
     </div>
